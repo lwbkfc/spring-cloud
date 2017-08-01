637bcd30f52360f98cae251db1b8a22db7496235
lwbkfc <lwb207628@163.com>
2017-08-01 17:58:44 +0800

    dddddddd

--- src/test/java/com/lin/cloud/controller/HelloApplicationTests.java
+++ src/test/java/com/lin/cloud/controller/HelloApplicationTests.java
@@ -1,9 +1,14 @@
 package com.lin.cloud.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lin.cloud.HelloApplication;
+import com.lin.cloud.config.MyProps;
+import com.lin.cloud.config.Sender;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -35,5 +40,24 @@ public class HelloApplicationTests {
     @Test
     public void hello() throws Exception {
         ResultActions perform = mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().string(equalTo("Hello World")));
+    }
+
+    @Autowired
+    private MyProps myProps;
+
+    @Test
+    public void propsTest() throws JsonProcessingException {
+        System.out.println("simpleProp: " + myProps.getSimpleProp());
+        System.out.println("arrayProps: " + JSONObject.toJSONString(myProps.getArrayProps()));
+        System.out.println("listProp1: " +  JSONObject.toJSONString(myProps.getListProp1()));
+        System.out.println("listProp2: " +  JSONObject.toJSONString(myProps.getListProp2()));
+        System.out.println("mapProps: " +  JSONObject.toJSONString(myProps.getMapProps()));
+    }
+
+    @Autowired
+    private Sender sender;
+    @Test
+    public void hello2() throws Exception {
+        sender.send();
     }
 }
